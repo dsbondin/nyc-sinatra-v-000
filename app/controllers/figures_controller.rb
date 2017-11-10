3472164f189f3b1badaf10ca5c9a85073b5961bb
Denis Bondin <dsbondin@gmail.com>
2017-11-10 00:29:46 +0000

    post /firures

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,14 @@
 class FiguresController < ApplicationController
 
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    figure = Figure.new(params[:figure][:name])
+  end
+
 end
